25dfc7ba955301d80b0cddb20bba63d1d919f68b
maclyle <maclyle32@gmail.com>
2020-06-15 21:44:44 -0700

    Home cleaned. Initial timer functionality on WorkoutView

--- app/src/main/java/com/example/intervaltimer/MainActivity.java
+++ app/src/main/java/com/example/intervaltimer/MainActivity.java
@@ -1,40 +1,18 @@
 package com.example.intervaltimer;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.os.Handler;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.os.SystemClock;
+
 
 
 public class MainActivity extends AppCompatActivity {
-
-    // Vars for basic timer
-    //Declare Vars used
-    TextView display;
-    Button start, stop, reset, set;
-    Handler handler;
-    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
-    int inputSeconds, Seconds, Minutes, MilliSeconds, SetTime ;
-    EditText setSec;
-    //Timer t1;
-
-    // Home screen vars
-    MediaPlayer shortBeep; // Probably needs to be created in onCreate thus still in Home activity
-    //Button newWorkout;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,142 +20,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        // Home screen vars and code #############################################
-        shortBeep = MediaPlayer.create(this, R.raw.short_beep01);
-
-        //newWorkout = findViewById(R.id.newWorkout);
-
-
-        // Timer Vars and code ####################################################
-        // Create the buttons and timer in code land to match layout land
-        display = (TextView)findViewById(R.id.display);
-        start = (Button)findViewById(R.id.startButton);
-        stop  = (Button)findViewById(R.id.stopButton);
-        reset = (Button)findViewById(R.id.resetButton);
-        set = (Button)findViewById(R.id.setTimer);
-        handler = new Handler();
-        setSec = (EditText)findViewById(R.id.setSeconds);
-
-        //t1 = new Timer("Test", 0, 10);
-
-//        set.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//        });
-
-        // Ex. use number pad ENTER/Done click to do an action
-        setSec.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                // Check if null entry
-                if (setSec.getText().toString().trim().length() == 0) {
-                    inputSeconds = 0;
-                } else {
-                    inputSeconds = Integer.valueOf(setSec.getText().toString());
-                }
-
-                SetTime = inputSeconds;
-                TimeBuff = inputSeconds * 1000; //TimeBuff in millisec
-
-                Seconds = inputSeconds % 60 ;
-                Minutes =  inputSeconds / 60 ;
-                MilliSeconds = 0 ;
-
-                display.setText("" + String.format("%02d", Minutes) + ":"
-                        + String.format("%02d", Seconds) + "."
-                        + String.format("%03d", MilliSeconds));
-
-                stop.setEnabled(true);
-                return false;
-            }
-        });
-
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                StartTime = SystemClock.elapsedRealtime();
-                handler.postDelayed(runnable, 0);
-
-                reset.setEnabled(false);
-                set.setEnabled(false);
-                stop.setEnabled(true);
-
-            }
-        });
-
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                TimeBuff -= MillisecondTime;
-
-                handler.removeCallbacks(runnable);
-
-                reset.setEnabled(true);
-                set.setEnabled(true);
-                stop.setEnabled(false);
-
-            }
-        });
-
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                MillisecondTime = 0L ;
-                StartTime = 0L ;
-                TimeBuff = SetTime * 1000;
-                UpdateTime = 0L ;
-
-                Seconds = SetTime % 60;
-                Minutes = SetTime / 60;
-                MilliSeconds = 0 ;
-
-                display.setText("" + String.format("%02d", Minutes) + ":"
-                        + String.format("%02d", Seconds) + "."
-                        + String.format("%03d", MilliSeconds));
-
-                stop.setEnabled(true);
-            }
-        });
-
     }
 
-    // Countdown Runnable
-    public Runnable runnable = new Runnable() {
 
-        public void run() {
-
-            MillisecondTime = SystemClock.elapsedRealtime() - StartTime;
-            UpdateTime = TimeBuff - MillisecondTime;
-
-            // Check if timer is at 0 or close enough :/ (hundreth sec accuracy I think)
-            if (UpdateTime <= 0) {
-                shortBeep.start();
-
-                display.setText("00:00.000");
-                handler.removeCallbacks(runnable);
-
-                // Button toggle on/off
-                reset.setEnabled(true);
-                set.setEnabled(true);
-                stop.setEnabled(false);
-            } else { // Continue countdown
-                Seconds = (int) (UpdateTime / 1000);
-                Minutes = Seconds / 60;
-                Seconds = Seconds % 60;
-                MilliSeconds = (int) (UpdateTime % 1000);
-                display.setText("" + String.format("%02d", Minutes) + ":"
-                        + String.format("%02d", Seconds) + "."
-                        + String.format("%03d", MilliSeconds));
-                handler.postDelayed(this, 0);
-            }
-        }
-
-    };
-
+    // Called onClick of the newWorkout Button
     public void launchNewWorkout(View view) {
         Intent intent = new Intent(this, WorkoutView.class );
         startActivity(intent);
--- app/src/main/java/com/example/intervaltimer/WorkoutView.java
+++ app/src/main/java/com/example/intervaltimer/WorkoutView.java
@@ -1,5 +1,6 @@
 package com.example.intervaltimer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,13 +10,28 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class WorkoutView extends AppCompatActivity implements NewTimerDialog.NewTimerDialogListener {
 
-    TextView currentTimer, currentName;
+    TextView currentTimerDisplay, currentNameDisplay;
     Workout workout;
+    ToggleButton startStop;
+
+    // Vars for basic timer
+    //Declare Vars used
+    Handler handler;
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
+    int Seconds, Minutes, MilliSeconds ;
+    MediaPlayer shortBeep;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +44,33 @@ public class WorkoutView extends AppCompatActivity implements NewTimerDialog.New
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
 
-        currentTimer = findViewById(R.id.topTimerDisplay);
-        currentName = findViewById(R.id.currentName);
+        // Timer Vars and code ####################################################
+        // Create the buttons and timer in code land to match layout land
+        handler = new Handler();
+        shortBeep = MediaPlayer.create(this, R.raw.short_beep01);
 
+        currentTimerDisplay = findViewById(R.id.topTimerDisplay);
+        currentNameDisplay = findViewById(R.id.currentName);
         // For New assuming only new workouts.
         // TODO: expand to accept saved workouts
         workout = new Workout();
+
+        startStop = findViewById(R.id.startStopButton);
+
+        startStop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Timer should be running (ie start pressed)
+                    StartTime = SystemClock.elapsedRealtime();
+                    handler.postDelayed(runnable, 0);
+                } else {
+                    // Timer should be stopped (Stop pressed)
+                    TimeBuff -= MillisecondTime;
+                    handler.removeCallbacks(runnable);
+                }
+            }
+        });
 
         FloatingActionButton newTimer = findViewById(R.id.newTimer);
         newTimer.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +83,33 @@ public class WorkoutView extends AppCompatActivity implements NewTimerDialog.New
         });
     }
 
+    // Countdown Runnable
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+            // TODO: Find place to set initial time buffer
+            MillisecondTime = SystemClock.elapsedRealtime() - StartTime;
+            UpdateTime = TimeBuff - MillisecondTime;
+
+            // Check if timer is at 0 or close enough :/ (hundreth sec accuracy I think)
+            if (UpdateTime <= 0) {
+                shortBeep.start();
+                currentTimerDisplay.setText("00:00.000");
+                handler.removeCallbacks(runnable);
+            } else { // Continue countdown
+                Seconds = (int) (UpdateTime / 1000);
+                Minutes = Seconds / 60;
+                Seconds = Seconds % 60;
+                MilliSeconds = (int) (UpdateTime % 1000);
+                currentTimerDisplay.setText("" + String.format("%02d", Minutes) + ":"
+                        + String.format("%02d", Seconds) + "."
+                        + String.format("%03d", MilliSeconds));
+                handler.postDelayed(this, 0);
+            }
+        }
+
+    };
+
     public void openTimerCreation() {
         NewTimerDialog timerDialog = new NewTimerDialog();
         timerDialog.show(getSupportFragmentManager(), "Timer creation");
@@ -53,11 +117,7 @@ public class WorkoutView extends AppCompatActivity implements NewTimerDialog.New
 
     @Override
     public void applyTexts(String name, int minutes, int seconds) {
-        // First need to make a new timer obj. and add to workout
-
-        //workout.add(newTimer);
-//        // Update the Display based on workout list
-//
+        // Relic method that may be used later.
     }
 
     public void addTimer(Timer timer) {
@@ -65,8 +125,9 @@ public class WorkoutView extends AppCompatActivity implements NewTimerDialog.New
         workout.add(timer);
         // Display current timer in list
         Timer timerNow = workout.currentTimer();
-        currentTimer.setText("" + String.format("%02d", timerNow.Minutes) + ":" + String.format("%02d",timerNow.Seconds) + ".000");
-        currentName.setText(timerNow.Name);
+        currentTimerDisplay.setText("" + String.format("%02d", timerNow.Minutes) + ":" + String.format("%02d",timerNow.Seconds) + ".000");
+        currentNameDisplay.setText(timerNow.Name);
+        TimeBuff = (timerNow.Minutes * 60 + timerNow.Seconds) * 1000;
 
     }
 }
